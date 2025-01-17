0bb5a0deea20b88eed536b9820bf20051849c278
vikaskalyan-rgb <vikaskalyan1102@gmail.com>
2025-01-18 04:43:52 +0530

    gap fix

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -106,7 +106,7 @@ Message: ${formData.message}
   return (
     <section
       id="contact"
-      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
+      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-1 md:gap-4 relative"
     >
       <div className="bg-[radial-gradient( from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
